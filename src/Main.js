c31b054b2686953878804da16e92ec894444a9fa
Krzysztof Piatkowski <kpi@trifork.com>
2018-01-10 11:43:46 +0100

    Added components

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+import MovieList from "./MovieList"
+import Footer from "./Footer"
+
+
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div id="main">
+                <NavigationBar id="navigationBar" />
+                <h1 id="main-header">{this.props.title}</h1>
+                <MovieList />
+                <Footer />
+            </div>
+        )
+    }
+}
---
+++ src/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class NavigationBar extends React.Component {
+    render() {
+        return (
+            <div id="navigation-bar"/>
+        )
+    }
+}
