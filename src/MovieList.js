e35f21a38b3b57f9ae7b6bfde7fc613fd401953b
Martin Rasmussen <mra@trifork.com>
2018-01-11 08:59:12 +0100

    Create movie list, and attach event listener

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,9 +1,21 @@
 import React from "react"
 
 export default class MovieList extends React.Component {
+    movies = require("./resources/movies.json")
+
+    onClick(movie) {
+        alert(`You just clicked ${movie.title}`)
+    }
+
     render() {
         return (
-            <div id="movie-list" />
+            <div id="movie-list">
+                {
+                    this.movies.map(movie => {
+                        return <img src={movie.imageUrl} key={movie.id} alt={movie.description} onClick={() => this.onClick(movie)}/>
+                    })
+                }
+            </div>
         )
     }
 }
