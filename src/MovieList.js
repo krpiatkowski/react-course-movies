e8aa495781b2772791cd7fb0233752e47b27231a
Krzysztof Piatkowski <kpi@trifork.com>
2018-01-12 13:45:36 +0100

    Added fix to state

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,25 +2,31 @@ import React from "react"
 import Axios from "axios"
 
 export default class MovieList extends React.Component {
-    movies = []
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies : []
+        }
+    }
 
-    componentWillMount(){
+    componentWillMount() {
         Axios.get("http://localhost:3001/movies").then((res) => {
-            this.movies = res.data
-            this.forceUpdate()
+            this.setState({
+                movies: res.data
+            })
         })
     }
 
     onClick(movie) {
-        alert(`You just clicked ${movie.title}`)
+                alert(`You just clicked ${movie.title}`)
     }
 
     render() {
         return (
             <div id="movie-list">
                 {
-                    this.movies.map(movie => {
-                        return <img src={movie.imageUrl} key={movie.id} alt={movie.description} onClick={() => this.onClick(movie)}/>
+                    this.state.movies.map(movie => {
+                        return <img src={movie.imageUrl} key={movie.id} alt={movie.description} onClick={() => this.onClick(movie)} />
                     })
                 }
             </div>
