480be2ee9f45534e3963a1bbf5af4abf22fdaef1
Krzysztof Piatkowski <kpi@trifork.com>
2018-01-12 12:46:55 +0100

    Renaming a bit

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default class MovieList extends React.Component {
+export default class Content extends React.Component {
     render() {
         return (
             <div id="content" />
