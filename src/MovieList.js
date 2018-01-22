1ed039cfef2fa42dab15b086e6def66ae5f87e4a
Krzysztof Piatkowski <kpi@trifork.com>
2018-01-22 08:20:55 +0100

    Better loading

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,7 +7,7 @@ export default class MovieList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            movies : []
+            movies: null
         }
     }
 
@@ -20,18 +20,22 @@ export default class MovieList extends React.Component {
     }
 
     onClick(movie) {
-        if(this.props.onSelectMovie) {
+        if (this.props.onSelectMovie) {
             this.props.onSelectMovie(movie)
         }
     }
 
     render() {
         return (
-            <div id="movie-list">
-                {
-                    this.state.movies.map(movie => <Movie movie={movie} onClick={() => this.onClick(movie)} key={movie.id}/>)
-                }
-            </div>
+            this.state.movies ? (
+                <div id="movie-list">
+                    {
+                        this.state.movies.map(movie => <Movie movie={movie} onClick={() => this.onClick(movie)} key={movie.id} />)
+                    }
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )
         )
     }
 }
